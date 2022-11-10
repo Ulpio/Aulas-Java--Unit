7338fc257377c29c643c8406fc2a736981072be6
ulpio <ulpionetto0@gmail.com>
2022-11-10 10:53:20 -0300

    Criado o vetor que percorre os objetos

--- Aula 9_11/src/Teste.java
+++ Aula 9_11/src/Teste.java
@@ -3,11 +3,18 @@ import java.util.Vector;
 public class Teste {
     public static void main(String[] args) {
         Circulo circulo = new Circulo(5);
-        System.out.println(circulo.areaCalculada()+"cm²");
+        //System.out.println(circulo.areaCalculada()+"cm²");
         Quadrado quadrado = new Quadrado(3);
-        System.out.println(quadrado.areaCalculada()+"cm²");
+        //System.out.println(quadrado.areaCalculada()+"cm²");
         Retangulo retangulo = new Retangulo(5, 2);
-        System.out.println(retangulo.areaCalculada()+"cm²");
+        //System.out.println(retangulo.areaCalculada()+"cm²");
+        Vector<AreaCalculavel> area = new Vector<>();
+        area.add(retangulo);
+        area.add(circulo);
+        area.add(quadrado);
+        for (AreaCalculavel x: area){
+            System.out.println(x.areaCalculada()+"cm²");
+        }
     }
     
 }
