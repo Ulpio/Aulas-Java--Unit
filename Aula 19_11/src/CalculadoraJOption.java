786ad9dc57838f6de76b108dcc8ae0a036c1868f
ulpio <ulpionetto0@gmail.com>
2022-11-23 19:21:39 -0300

    Calculadora App PopUp

--- Aula 19_11/src/CalculadoraJOption.java
+++ Aula 19_11/src/CalculadoraJOption.java
@@ -4,16 +4,12 @@ public class CalculadoraJOption {
     public static void main(String[] args) {
         float num1,num2;
         String operacao;
-
         num1 = Float.parseFloat(JOptionPane.showInputDialog("Insira o primeiro valor"));
         num2 = Float.parseFloat(JOptionPane.showInputDialog("Insira o segundo valor"));
-
         operacao = JOptionPane.showInputDialog("insira a operação (+,-,*,/)");
- 
-
         switch(operacao){
             case "+":{
-                JOptionPane.showMessageDialog(null, "O resultado da soma é:" + somar(num1, num2), "Resultado", 1);
+                JOptionPane.showMessageDialog(null, "O resultado da soma é:" + somar(num1, num2), "Resultado", 0);
                 break;
             }
             case "-":{
@@ -21,17 +17,15 @@ public class CalculadoraJOption {
                 break;
             }
             case "*":{
-                JOptionPane.showMessageDialog(null, "O resultado da multiplicação  é:" + multiplicar(num1, num2), "Resultado", 1);
+                JOptionPane.showMessageDialog(null, "O resultado da multiplicação  é:" + multiplicar(num1, num2), "Resultado", 2);
                 break;
             }
             case "/":{
-                JOptionPane.showMessageDialog(null, "O resultado da divisão é:" + dividir(num1, num2), "Resultado", 1);
+                JOptionPane.showMessageDialog(null, "O resultado da divisão é:" + dividir(num1, num2), "Resultado", 3);
                 break;
             }
             default:{JOptionPane.showMessageDialog(null, "Operação inválida", "Erro", 0);
-
             }
-            
         }  
     }
     public static float somar(float num1,float num2){
